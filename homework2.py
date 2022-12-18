f50ae2a8d4a6c177b5836c62c28a5985d6a9c84d
NONEG1 <120308902+NONEG1@users.noreply.github.com>
2022-12-19 03:07:27 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+full_name = ("giorgi chanturidze")
+print(full_name[10])
+print(full_name[-1:-12:-1])
+print(full_name.upper())
+print(full_name[5]+"asamani")
+name2 = "     chrichina       "
+print(name2.replace(" ", "%"))
+name3 = "Plato "
+print(name3[:6]+"was ancient Greek philosoper")
