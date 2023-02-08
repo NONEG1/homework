304da068e08ab573e199548ecfcec6545ca3dfdc
NONEG1 <120308902+NONEG1@users.noreply.github.com>
2023-02-08 20:53:38 +0400

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,43 @@
+#name = "გიორგი"
+#print(len(name))
+
+#my_sentence = "Hello, this lesson will start on February 19. Please attend it. მადლობა!"
+#print(my_sentence)
+
+#if 10>5:
+    #print("it's true")
+    #tab-ის საშუალებით მოვახდინეთ ინდენტაცია (indentation)
+
+full_name = "giorge tchanturidze"
+#print(len(full_name))
+
+#print(full_name[0])
+
+#print("giorge" in full_name)
+#print("giorge" not in full_name)
+
+# print(full_name[0:7])
+# print(full_name[0:7:2]) #start:finish:step
+#
+# print(full_name[:7])
+# print(full_name[8:])
+
+# print(full_name[:-3] + "dze")
+
+#სტრინგებს აქვთ მეთოდი
+# 1) სტრინგ.upper()
+# 2)Len(სტრინგ)
+
+print(full_name.upper())
+
+full_name2 = "GIORGE TCHANTURIDZE"
+print(full_name2.lower())
+
+full_name3 = "    GIORGE    "
+print(full_name3.strip())
+
+print(full_name3.replace(" ", "-"))
+print(full_name3.replace("I", "$"))
+
+
+
